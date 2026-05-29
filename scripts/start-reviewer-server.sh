42cf3d02f1e83c07522ce0c5d0d0e25484c0050f
Alex Remington <7646443+alexremington@users.noreply.github.com>
2026-05-29 09:21:11 -0400

    Add Politico logo to duplicate header

--- scripts/start-reviewer-server.sh
+++ scripts/start-reviewer-server.sh
@@ -96,6 +96,8 @@ sync_static_assets() {
   copy_static_asset "${PROJECT_DIR}/app.js" "${STATIC_DIR}/app.js"
   copy_static_asset "${PROJECT_DIR}/matching-worker.js" "${STATIC_DIR}/matching-worker.js"
   copy_static_asset "${PROJECT_DIR}/styles.css" "${STATIC_DIR}/styles.css"
+  /bin/mkdir -p "${STATIC_DIR}/assets"
+  copy_static_asset "${PROJECT_DIR}/assets/politico-logo.svg" "${STATIC_DIR}/assets/politico-logo.svg"
   /bin/mkdir -p "${STATIC_DIR}/vendor/managed-app/css"
   /bin/mkdir -p "${STATIC_DIR}/vendor/managed-app/scripts"
   copy_static_asset "${PROJECT_DIR}/vendor/managed-app/css/managed-app-base.css" "${STATIC_DIR}/vendor/managed-app/css/managed-app-base.css"
